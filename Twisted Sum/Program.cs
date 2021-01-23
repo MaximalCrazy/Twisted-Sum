ed498bec3877204637a4659aa262b452108d21d7
Timo <timofalkenhain@gmail.com>
2021-01-23 15:04:03 +0100

    Twisted Sum Aufgabenstellung
    https://www.codewars.com/kata/527e4141bb2ea5ea4f00072f/train/csharp

---
+++ Twisted Sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Twisted_Sum
+{
+    // https://www.codewars.com/kata/527e4141bb2ea5ea4f00072f/train/csharp
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TwistedSum.Solution(4);
+            TwistedSum.Solution(10);
+            TwistedSum.Solution(12);
+        }
+    }
+
+    public class TwistedSum
+    {
+        public static long Solution(long n)
+        {
+            return 0;
+        }
+    }
+}
